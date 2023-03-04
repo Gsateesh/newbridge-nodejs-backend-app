5042a60470fe5e0df3c8f0eb28403738fb97e209
sateesh Gundeboina <sateesh.gundeboina@techgrit.com>
2023-03-04 23:37:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 80;
+
+
+app.get("/", (req, res) => {
+    res.send("Oops something went wrong.")
+})
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log("error occurred", err);
+    }
+    console.log("Server started ..", PORT);
+})
+module.exports = app;
