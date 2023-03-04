c5b1dd58884ff41227b6bb5f881f07863ae597d3
sateesh Gundeboina <sateesh.gundeboina@techgrit.com>
2023-03-05 00:05:41 +0530

    cors added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 80;
