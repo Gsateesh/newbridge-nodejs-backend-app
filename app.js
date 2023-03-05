96763a67f9532ffd97dd6d1df7f041312eaacd7f
sateesh Gundeboina <sateesh.gundeboina@techgrit.com>
2023-03-05 18:22:27 +0530

    cors added

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
 
 mongoose.connect("mongodb+srv://salesteamnewbridge:mseWtJrXdhhd53U4@cluster0.elwm8de.mongodb.net/service-requests");
 
@@ -11,8 +14,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 
-app.use(cors());
-app.use(bodyParser.json());
+
 
 const service = require('./routes/routes.service');
 app.use('/service', service);
